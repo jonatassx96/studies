7c19f9b011e5f450cb274511e63fbc756399818e
Jonatas Xavier <81437599+jonatassx96@users.noreply.github.com>
2024-12-21 23:28:50 -0300

    Programming logic with java

---
+++ programming-logic-with-java/classe-string.java
@@ -0,0 +1,8 @@
+// Classe String
+class Main {
+    public static void main(String[] args) {
+        String name = "Jônatas Xavier";
+
+        System.out.println(name.contains("Xavier"));
+    }
+}
---
+++ programming-logic-with-java/variaveis-e-tipos-de-dados.java
@@ -0,0 +1,16 @@
+// Variáveis e Tipos de Dados
+
+class Main {
+    public static void main(String[] args) {
+        byte valoresInteiros;
+        short valoresInteriros2;
+        int numero;
+        long numeroInteriro;
+        boolean trueOrFalse;
+        char caracteresIndividuais;
+        float saldo;
+        String abacaxi;
+        double numeroEmPontoFlutuante;
+
+    }
+}
