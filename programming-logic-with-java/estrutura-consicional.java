ab4111d3cf7501e96b695e117a1e7d9b53a3a46a
Jonatas Xavier <81437599+jonatassx96@users.noreply.github.com>
2024-12-25 22:55:12 -0300

    Add file "estrutura-condicional.java"

---
+++ programming-logic-with-java/estrutura-consicional.java
@@ -0,0 +1,21 @@
+// Estrutura condicional
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args) {
+        Random gerador = new Random();
+        int numeroAleatorio = gerador.nextInt(100);
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Adivinhe o número que estou pensando");
+        int numero = entrada.nextInt();
+
+        if(numero == numeroAleatorio){
+            System.out.println("Parabéns, você acertou eu pensei no " + numeroAleatorio)
+        }
+        else{
+            System.out.println("Você errou, eu pensei no " + numeroAleatorio)
+        }
+    }
+}
